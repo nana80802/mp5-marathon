0719743ce4e4277cfa422b85ea4bf57a42865b09
nana80802 <miss_nisreen2000@yahoo.com>
2018-03-29 15:55:54 +0200

    create the marathon App structure

---
+++ src/main/java/at/refugeescode/mp5marathon/Mp5MarathonApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp5marathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp5MarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp5MarathonApplication.class, args);
+	}
+}
