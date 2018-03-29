df23a36daf52e01a6f1b55f6b2c55622c0e4a2f2
nana80802 <miss_nisreen2000@yahoo.com>
2018-03-29 17:49:39 +0200

    finished with Maraton

---
+++ src/main/java/at/refugeescode/mp5marathon/endpoint/Winner.java
@@ -0,0 +1,28 @@
+package at.refugeescode.mp5marathon.endpoint;
+
+import at.refugeescode.mp5marathon.model.Runner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/winner")
+public class Winner {
+    private List<Runner> runners= Runners.runners;
+
+    @GetMapping
+    Runner displayWinner(){
+        Optional<Runner> winner = runners.stream()
+                //.min(Comparator.comparing(Runner::getTime))
+                .sorted((e1, e2) -> e1.getTime().compareTo(e2.getTime()))
+                .findFirst();
+        if (winner.isPresent()){
+            return winner.get();
+        }
+        return new Runner("no runner");
+    }
+}
